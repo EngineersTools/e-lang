f09c3aeeac2745500e29fd835d0a1dde42920d1e
Carlos Gamez <carlos.gamez@novaenergy.consulting>
2024-05-23 20:50:55 +0800

    Updated 'vite' version and fixed type issue in runVariableDeclaration

--- src/interpreter/runVariableDeclaration.ts
+++ src/interpreter/runVariableDeclaration.ts
@@ -1,7 +1,9 @@
 import { interruptAndCheck } from "langium";
 import {
-    ConstantDeclaration,
-    MutableDeclaration
+  ConstantDeclaration,
+  MutableDeclaration,
+  isExpression,
+  isLambdaDeclaration,
 } from "../language/generated/ast.js";
 import { isAssignable } from "../language/type-system/assignment.js";
 import { inferType } from "../language/type-system/infer.js";
@@ -16,27 +18,31 @@ export async function runVariableDeclaration(
   await interruptAndCheck(context.cancellationToken);
 
   if (statement.assignment && statement.value) {
-    const type = inferType(statement, context.types);
-    const valueType = inferType(statement.value, context.types);
+    if (isExpression(statement.value)) {
+      const type = inferType(statement, context.types);
+      const valueType = inferType(statement.value, context.types);
 
-    context.types.setVariableType(statement.name, type);
+      context.types.setVariableType(statement.name, type);
 
-    if (!isAssignable(type, valueType)) {
-      throw new AstNodeError(
-        statement,
-        "Invalid types between variable and its assigned value."
-      );
+      if (!isAssignable(type, valueType)) {
+        throw new AstNodeError(
+          statement,
+          "Invalid types between variable and its assigned value."
+        );
+      }
+
+      const value = statement.value
+        ? await runExpression(statement.value, context)
+        : null;
+
+      context.variables.push(statement.name, value);
+    } else {
+      // The variable hasn't been assigned a value when declared
+      // In Elang, any variable that doesn't have a value
+      // is assigned the value of null
+      context.variables.push(statement.name, null);
     }
-
-    const value = statement.value
-      ? await runExpression(statement.value, context)
-      : null;
-
-    context.variables.push(statement.name, value);
-  } else {
-    // The variable hasn't been assigned a value when declared
-    // In Elang, any variable that doesn't have a value
-    // is assigned the value of null
-    context.variables.push(statement.name, null);
+  } else if (isLambdaDeclaration(statement.value)) {
+    
   }
 }
