e645ac56eff937b780d219f34eb52a5952245c3c
Carlos Gamez <carlos.gamez@novaenergy.consulting>
2024-07-12 19:21:29 +0800

    chore: Updated language names in example notebooks

--- src/extension/main.ts
+++ src/extension/main.ts
@@ -1,51 +1,92 @@
-import type { LanguageClientOptions, ServerOptions} from 'vscode-languageclient/node.js';
-import type * as vscode from 'vscode';
-import * as path from 'node:path';
-import { LanguageClient, TransportKind } from 'vscode-languageclient/node.js';
+import * as path from "node:path";
+import * as vscode from "vscode";
+import type {
+  LanguageClientOptions,
+  ServerOptions,
+} from "vscode-languageclient/node.js";
+import { LanguageClient, TransportKind } from "vscode-languageclient/node.js";
+import { ELangNotebookKernel } from "../notebook/notebook-kernel.js";
+import { ELangNotebookSerializer } from "../notebook/notebook-serialiser.js";
 
 let client: LanguageClient;
 
 // This function is called when the extension is activated.
 export function activate(context: vscode.ExtensionContext): void {
-    client = startLanguageClient(context);
+  client = startLanguageClient(context);
+
+  context.subscriptions.push(
+    vscode.workspace.registerNotebookSerializer(
+      "e-lang-notebook",
+      new ELangNotebookSerializer(),
+      { transientOutputs: true }
+    ),
+    new ELangNotebookKernel()
+  );
 }
 
 // This function is called when the extension is deactivated.
 export function deactivate(): Thenable<void> | undefined {
-    if (client) {
-        return client.stop();
-    }
-    return undefined;
+  if (client) {
+    return client.stop();
+  }
+  return undefined;
 }
 
 function startLanguageClient(context: vscode.ExtensionContext): LanguageClient {
-    const serverModule = context.asAbsolutePath(path.join('out', 'language', 'main.cjs'));
-    // The debug options for the server
-    // --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging.
-    // By setting `process.env.DEBUG_BREAK` to a truthy value, the language server will wait until a debugger is attached.
-    const debugOptions = { execArgv: ['--nolazy', `--inspect${process.env.DEBUG_BREAK ? '-brk' : ''}=${process.env.DEBUG_SOCKET || '6009'}`] };
+  const serverModule = context.asAbsolutePath(
+    path.join("out", "language", "main.cjs")
+  );
+  // The debug options for the server
+  // --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging.
+  // By setting `process.env.DEBUG_BREAK` to a truthy value, the language server will wait until a debugger is attached.
+  const debugOptions = {
+    execArgv: [
+      "--nolazy",
+      `--inspect${process.env.DEBUG_BREAK ? "-brk" : ""}=${
+        process.env.DEBUG_SOCKET || "6009"
+      }`,
+    ],
+  };
 
-    // If the extension is launched in debug mode then the debug server options are used
-    // Otherwise the run options are used
-    const serverOptions: ServerOptions = {
-        run: { module: serverModule, transport: TransportKind.ipc },
-        debug: { module: serverModule, transport: TransportKind.ipc, options: debugOptions }
-    };
+  // If the extension is launched in debug mode then the debug server options are used
+  // Otherwise the run options are used
+  const serverOptions: ServerOptions = {
+    run: { module: serverModule, transport: TransportKind.ipc },
+    debug: {
+      module: serverModule,
+      transport: TransportKind.ipc,
+      options: debugOptions,
+    },
+  };
 
-    // Options to control the language client
-    const clientOptions: LanguageClientOptions = {
-        documentSelector: [{ scheme: 'file', language: 'e-lang' }]
-    };
+  const fileSystemWatcher =
+    vscode.workspace.createFileSystemWatcher("**/*.{el,elnb}");
+  context.subscriptions.push(fileSystemWatcher);
 
-    // Create the language client and start the client.
-    const client = new LanguageClient(
-        'e-lang',
-        'e-lang',
-        serverOptions,
-        clientOptions
-    );
+  // Options to control the language client
+  const clientOptions: LanguageClientOptions = {
+    documentSelector: [
+      { scheme: "file", language: "e-lang" },
+      {
+        scheme: "vscode-notebook-cell",
+        language: "e-lang",
+      },
+    ],
+    synchronize: {
+      // Notify the server about file changes to files contained in the workspace
+      fileEvents: fileSystemWatcher,
+    },
+  };
 
-    // Start the client. This will also launch the server
-    client.start();
-    return client;
+  // Create the language client and start the client.
+  const client = new LanguageClient(
+    "e-lang",
+    "e-lang",
+    serverOptions,
+    clientOptions
+  );
+
+  // Start the client. This will also launch the server
+  client.start();
+  return client;
 }
